4b3c087bf74f3b79c386522b24a1066cfa29eb2b
vsvshinde <vishal.shinde@codeastu.com>
2021-02-08 14:25:39 +0530

    Removes todo Application code.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./stylesheet/reset.css";
-import "./stylesheet/style.css";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import AddTodo from "./components/AddTodo";
 import FlightApp from "./components/FlightApp";
-import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <FlightApp />
-  </Provider>,
-  document.querySelector(".container")
-);
+ReactDOM.render(<FlightApp />, document.querySelector(".container"));
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import ToDoItem from './todoReducer';
-
-export default combineReducers({
-  ToDoItem,
-});
